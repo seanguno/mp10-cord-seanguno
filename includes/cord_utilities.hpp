181bdab35790e345cabe4b66ff0ffe07a737b815
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:13:51 -0500

    Update cord_utilities.hpp

--- includes/cord_utilities.hpp
+++ includes/cord_utilities.hpp
@@ -4,11 +4,13 @@
 #include "cord.hpp"
 #include "shared_pointer.hpp"
 
-// SharedPointer<Cord> ConcatCords(const SharedPointer<Cord>& left_cord,
+//  SharedPointer<Cord> ConcatCords(const SharedPointer<Cord>& left_cord,
 //                                 const SharedPointer<Cord>& right_cord);
 
-// SharedPointer<Cord> SubString(SharedPointer<Cord> curr_cord,
-//                               unsigned int lower_idx,
-//                               unsigned int upper_idx);
+SharedPointer<Cord> SubString(SharedPointer<Cord> curr_cord,unsigned int lower_idx, unsigned int upper_idx);
+
+SharedPointer<Cord> SubstringHelper(const SharedPointer<Cord>& curr_cord, unsigned int upper_idx, unsigned int lower_idx);
+
+SharedPointer<Cord> UnevenSubstring(const SharedPointer<Cord>& curr_cord,unsigned int upper_idx, unsigned int left_length, unsigned int lower_idx);
 
 #endif
