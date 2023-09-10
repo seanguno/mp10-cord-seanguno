0ebd63875e456f75d7bfbdfab08b228ae59510ca
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:15:34 -0500

    Update driver.cc

--- src/driver.cc
+++ src/driver.cc
@@ -4,7 +4,13 @@
 #include "cord.hpp"
 
 int main() {
+  SharedPointer<int> a = new int(22);
+  SharedPointer<int> a_copy;
+  a = a_copy;
   // SharedPointer<Cord> a = new Cord("CS_");
+  // SharedPointer<Cord> copy_a = a;
+  // auto copy_a_str = copy_a->ToString();
+  // std::cout << copy_a_str.Get() << std::endl;
   // SharedPointer<Cord> b = new Cord("128_");
   // SharedPointer<Cord> c = ConcatCords(a, b);
 
